b726598206bb103462f740a167828438c45bb4ec
unknown <tasbihaasim@gmail.com>
2024-04-04 16:49:00 +0200

    amsterdam projects added

--- tasbihaasim/src/Portfolio.js
+++ tasbihaasim/src/Portfolio.js
@@ -10,8 +10,35 @@ function Portfolio() {
       <a href="https://twitter.com/your_twitter_link"></a>
       <a href="https://linkedin.com/your_linkedin_link"></a>
     </div>
-  
-  <div class="projects">
+  <div/>
+
+    <div class="projects">
+    <div class="project">
+      <h2 className='Title'>Energy Consumption in Microservices Architectures</h2>
+      <p className='Desc'>Comprehensive Investigation of energy consumption and resource utilization of microservices architecture, focusing on a real-world application, the Train-Ticket Booking System. Highlighted the significant impact of Linux governors on energy consumption by Microservices.</p>
+      <div className='cd'>
+      <a className='Code' href="https://github.com/tasbihaasim/experiment-runner">Code</a>
+      <a className='Depl' href="https://drive.google.com/file/d/18kjN-oU8DspIG4L5e44W8MP0vQ-rU4PV/view">Documentation</a>
+      </div>
+    </div>
+
+    <div class="project">
+      <h2 className='Title'>Adaptation Strategy for Crowdsourced Navigation System (CrowdNav)</h2>
+      <p className='Desc'> Implemented self-adaptive system using MAPE-K model for dynamic traffic management. Utilized NSGA-2 algorithm and knowledge base for real-time adaptation. Deployed on Ubuntu using Docker for scalability.</p>
+      <div className='cd'>
+      <a className='Code' href="https://drive.google.com/file/d/1X6cePe_GCY49YasUd1uvz3LNxJuaJCmF/view">Documentation</a>
+      </div>
+    </div>
+
+    <div class="project">
+      <h2 className='Title'>Balancing Power and Performance: Energy Consumption Analysis of R-GCNs with Graph Summarization</h2>
+      <p className='Desc'> Conducted comprehensive analysis of energy consumption in relational graph convolutional networks (R-GCNs) with summarized graph representations. Utilized Code Carbon for precise energy consumption metrics.</p>
+      <div className='cd'>
+      <a className='Code' href="https://github.com/tasbihaasim/rgcn-on-summarized-graph">Code</a>
+      <a className='Depl' href="https://drive.google.com/file/d/1-FjrB9q8xG_CUNw_TZH0T8bXYY7-SgjU/view">Documentation</a>
+      </div>
+    </div>
+    
     <div class="project">
       <h2 className='Title'>GroupTherapy+</h2>
       <p className='Desc'>GroupTherapy+ is a full-stack chat application for online group therapy. The backend server uses Go and handles multiple instances for high concurrency. It broadcasts messages and alerts users when someone joins the chat room.</p>
@@ -28,6 +55,25 @@ function Portfolio() {
       <a className='Depl' href="https://moneexa.github.io/SusAFAnalysis/">Deployment</a>
       </div>
     </div>
+
+    <div class="project">
+      <h2 className='Title'>Reducing the Carbon Footprint of Artificial Intelligence</h2>
+      <p className='Desc'>Conducted extensive secondary research on energy-efficient practices in AI algorithms, including model size reduction and pruning. Produced a comprehensive report detailing approaches to optimize energy and power efficiency in AI applications.</p>
+      <div className='cd'>
+      <a className='Depl' href="https://drive.google.com/file/d/1GOhlI8AT4nqe38EOGjU8LNVF8d3HfJYO/view">Report</a>
+      </div>
+
+    </div>
+
+    <div class="project">
+      <h2 className='Title'>Quality Assessment of Intelligent Transport Services with ETCV Software System</h2>
+      <p className='Desc'>Evaluated and implemented static, dynamic, and deployment views of an AD vehicle software system using UML diagrams. Conducted quality assessment to ensure effective implementation of system architecture.</p>
+      <div className='cd'>
+      <a className='Depl' href="https://drive.google.com/file/d/1m7Z-_lhnOSdE1XYtPcaZpdLOk3eYu2WS/view">Report 1</a>
+      <a className='Depl' href="https://drive.google.com/file/d/1vGDUz-cV_lzwamheUNFhHpzrXPO7Gp7W/view">Report 2</a>
+      </div>
+    </div>
+
     <div class="project">
       <h2 className='Title'>Knowledge-Driven WhatsApp Based Financial Consultant</h2>
       <p className='Desc'>Developed Expert System for financial inclusion of local women with Knowledge Engineering using a novel knowledge graph approach.</p>
@@ -48,12 +94,12 @@ function Portfolio() {
       <p className='Desc'>Implementation of Q-learning algorithm and application on a given policy.</p>
       <div className='cd'>
       <a className='Code' href="https://github.com/tasbihaasim/Markov-Decision-Process">Code</a>
-      <a className='Depl' href="https://drive.google.com/drive/u/0/folders/1iYF4HKJPfVlDZ0V9wOo8ANoq2k-vxgZi">Documentation</a>
+      <a className='Depl' href="https://drive.google.com/file/d/1EX40wWjncmB9KU7ZrUxRMwcQagqbuiWq/view?usp=sharing">Documentation</a>
       </div>
     </div>
     <div class="project">
       <h2 className='Title'>Flood Management System using IOT</h2>
-      <p className='Desc'>This project focused on the analysis of data collected through IoT devices deployed in the rivers of Pakistan. The project utilized MQTT and COAP brokers for implementation.</p>
+      <p className='Desc'>Leveraged IoT devices and MQTT/CoAP brokers for continuous data collection. Processed data with Python scripts and InfluxDB for flood predictions. Implemented Twilio API for real-time SMS alerts.</p>
       <div className='cd'>
       <a className='Code' href="https://github.com/tasbihaasim/Flood-Management-System-using-IOT">Code</a>
       <a className='Depl' href="https://drive.google.com/file/d/1fk-kulqE7BE-GnRULFggYR4mDRGSmgq3/view">Deployment</a>
@@ -64,7 +110,7 @@ function Portfolio() {
       <h2 className='Title'>Adaptive Minimax</h2>
       <p className='Desc'>Experimented with different heuristics for the same algorithm. Devised two new implementations to achieve significant speed up over the existing minimax algorithm. Game of chess was used as a test bed. </p>
       <div className='cd'>
-      <a className='Code' href="https://drive.google.com/drive/u/0/folders/1ZqGYK5LQay6su62aVAq8dtgPMpnrFnmm">Code</a>
+      <a className='Code' href="https://drive.google.com/file/d/1y3dX9lxHUqd-v5VnijbGqlZrrN1DwMyT/view?usp=sharing">Documentation</a>
       </div>
       
     </div>
